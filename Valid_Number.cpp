23d3771772538c39e9de57d7f1bab684bab1088c
Jashanpreet singh <79403733+Jashanpsingh@users.noreply.github.com>
2023-03-08 12:32:40 +0530

    Update Valid_Number.cpp

--- Valid_Number.cpp
+++ Valid_Number.cpp
@@ -1,72 +1,36 @@
 class Solution {
 public:
-    bool isNumber(string s) {
-    int n = s.length();
-    int i = 0;
-    bool isNumeric = false;
-    bool hasE = false;
-    bool hasDecimal = false;
-    bool hasSign = false;
-    
-    // Skip leading spaces
-    while (i < n && s[i] == ' ') {
-        i++;
-    }
-    
-    // Check for sign
-    if (i < n && (s[i] == '+' || s[i] == '-')) {
-        hasSign = true;
-        i++;
-    }
-    
-    // Check for numeric part before decimal or exponent
-    while (i < n && isdigit(s[i])) {
-        isNumeric = true;
-        i++;
-    }
-    
-    // Check for decimal point and numeric part after decimal
-    if (i < n && s[i] == '.') {
-        hasDecimal = true;
-        i++;
-        while (i < n && isdigit(s[i])) {
-            isNumeric = true;
-            i++;
-        }
-    }
-    
-    // Check for exponent and numeric part after exponent
-    if (isNumeric && i < n && (s[i] == 'e' || s[i] == 'E')) {
-        hasE = true;
-        hasSign = false;
-        isNumeric = false;
-        i++;
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            hasSign = true;
-            i++;
-        }
-        while (i < n && isdigit(s[i])) {
-            isNumeric = true;
-            i++;
-        }
-    }
-    
-    // Skip trailing spaces
-    while (i < n && s[i] == ' ') {
-        i++;
-    }
-    
-    // The string is a valid number if all characters have been processed and there is at least one numeric character
-    return isNumeric && i == n;
-}
-
-int main() {
-    string s = "53.5e93";
-    if (isNumber(s)) {
-        cout << "The string is a valid number." << endl;
-    } else {
-        cout << "The string is not a valid number." << endl;
-    }
-    return 0;
+       bool isNumber(string s) {
+    bool digit=false,e=false,dot=false;
+    int sign=0;//for count  sign
+    int n=s.size()-1;
+    for(int i=0;i<=n;i++){
+        char ch=s[i];
+            // digit
+        if(ch>='0'&&ch<='9')
+                digit =true;
+            // sign
+        else if(ch=='+'||ch=='-'){
+             if(sign>1)return false;
+             else if(i>0&&s[i-1]!='e'&&s[i-1]!='E')return false;
+             else if(i==n)return false;
+                sign++;
+                }
+            //  dot
+        else if(ch=='.'){
+             if(e||dot)return false;
+             else if(i==n&&!digit)return false;  
+                dot=true;
+            }
+            //E or e
+        else if(ch=='e'||ch=='E'){
+             if(e||!digit||i==n)return false;
+                e=true;
+            }
+        else{//if element is other than all i.e='a'
+                return false;
+            } 
+        }    
+    return true;
 }
 };
