bea5fe0df5c07a7d242f26371fd3fe6c7bfe9347
Jashanpreet singh <79403733+Jashanpsingh@users.noreply.github.com>
2023-03-14 12:01:29 +0530

    Create Excel_Sheet_Column_title.java

---
+++ Excel_Sheet_Column_title.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        return columnNumber == 0 ? "" :  convertToTitle((columnNumber - 1) / 26) + (char) ('A' + ((columnNumber - 1) % 26));
+    }
+}
