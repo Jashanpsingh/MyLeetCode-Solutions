057854742e0a45bd05185757d83c5943f0987ccc
Jashanpreet singh <79403733+Jashanpsingh@users.noreply.github.com>
2023-03-27 11:16:05 +0530

    Create Decrypt_String_from_Alphabet_to_Integer_Mapping.java

---
+++ Decrypt_String_from_Alphabet_to_Integer_Mapping.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String freqAlphabets(String str) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = str.length() - 1; i >= 0; ) {
+        int encodedDigit = str.charAt(i) - '0';
+
+        int shift;
+        
+        if (encodedDigit >= 1 && encodedDigit <= 9) {
+            shift = 1;
+        }
+         else { // When # is encountered then calculate encoded number
+            encodedDigit = (str.charAt(i - 2) - '0') * 10 + (str.charAt(i - 1) - '0');
+            shift = 3;
+        }
+        
+        sb.append((char) ('a' + encodedDigit - 1));
+        i -= shift;
+    }
+    
+    return sb.reverse().toString(); // Reverse the string when string is decoded. 
+    } 
+}
